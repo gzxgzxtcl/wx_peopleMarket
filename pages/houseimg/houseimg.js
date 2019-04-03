2b2db3f538ff360c938ecbcbeb1df2e7ecdf7f12
97pkp <17865353357@163.com>
2019-04-03 18:22:31 +0800

    图片展示

--- pages/houseimg/houseimg.js
+++ pages/houseimg/houseimg.js
@@ -17,6 +17,13 @@ Page({
       this.setData({ isLeftSel: false, isRightSel: true })
     }
   },
+  // 获取大图
+  getBigImg(){
+    wx.previewImage({
+      current: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640', // 当前显示图片的http链接
+      urls: ['https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'] // 需要预览的图片http链接列表
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
