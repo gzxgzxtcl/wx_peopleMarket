ded119197a207d22c33d6085ef8db6f47b405abe
ISS\issuser <13313231519@163.com>
2019-04-04 18:25:02 +0800

    update

--- pages/cityList/cityList.js
+++ pages/cityList/cityList.js
@@ -5,62 +5,82 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cityListData: [{
+        key: 'b',
+        item: [{
+          id: 1,
+          name: '北京'
+        }]
+      },
+      {
+        key: 'c',
+        item: [{
+          id: 2,
+          name: '重庆'
+        }, {
+          id: 3,
+          name: '成都'
+        }, {
+          id: 4,
+          name: '长沙'
+        }]
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
