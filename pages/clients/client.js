5f27e8e2558268d9e407a9a985282e92821b2fd4
97pkp <17865353357@163.com>
2019-04-04 17:48:40 +0800

    2019-4-4 下午

--- pages/clients/client.js
+++ pages/clients/client.js
@@ -5,8 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showRight: false
+    showRight: false,
+    citySelArr:[true,false,false] ,  /*城市是否选中列表*/
+    planSelArr:[true,false,false,false,false],   /*进度是否选中*/
+    array: ['项目1', '项目2', '项目3', '项目4'],
+    itemPakerIndex: null,
+    dataIntervalStart:null,
+    dataIntervalEnd:null
+       
   },
+
+  //项目选择
+  bindPickerChange(e) {
+    this.setData({
+      itemPakerIndex: e.detail.value
+    })
+  },
+  // 时间区间选择
+  bindDateChangeStart(e) {
+    this.setData({
+      dataIntervalStart: e.detail.value
+    })
+  },
+  bindDateChangeEnd(e) {
+    this.setData({
+      dataIntervalEnd: e.detail.value
+    })
+  },
+
+
+
+
   // 遮罩弹出
   toggleRight() {
     this.setData({
@@ -19,12 +48,46 @@ Page({
       showRight: false
     });
   },
-  // 选择标签
-  selTag(e){
-    console.log(e)
+  // 选择城市标签
+  selCity(e){
+    let tagId=e.target.dataset.citytagid;
+    for (let i = 0; i < this.data.citySelArr.length;i++){
+      this.setData({ ['citySelArr['+i+']']: false })
+    }
+    this.setData({ ['citySelArr['+tagId+']']:true})
   },
-
-
+  // 选择进度标签
+  selPlan(e) {
+    if (e.target.dataset.plantagid===undefined) return
+    let tagId = e.target.dataset.plantagid;
+    for (let i = 0; i < this.data.planSelArr.length; i++) {
+      this.setData({ ['planSelArr[' + i + ']']: false })
+    }
+    this.setData({ ['planSelArr[' + tagId + ']']: true })
+  },
+  // 重置
+  reset(){
+    //城市重置
+    for(let i=0;i<this.data.citySelArr.length;i++){
+      if(i===0){
+        this.setData({ ['citySelArr[0]']: true });
+        continue;
+      }else{
+        this.setData({ ['citySelArr[' + i + ']']: false })
+      }
+    }
+    // 项目重置
+    this.setData({ itemPakerIndex: null})
+    this.setData({ dataIntervalStart: null, dataIntervalEnd: null})
+    // 进度重置
+    for (let i = 0; i < this.data.planSelArr.length; i++) {
+      if(i===0){
+        this.setData({ ['planSelArr[0]']: true });
+        continue;
+      }
+      this.setData({ ['planSelArr[' + i + ']']: false })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
