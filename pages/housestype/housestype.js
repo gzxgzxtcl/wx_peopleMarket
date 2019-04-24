1cecf11fcda91dd6a34877dd1387bfa2e0b89be4
97pkp <17865353357@163.com>
2019-04-24 20:16:47 +0800

    解决户型列表没有数据bug

--- pages/housestype/housestype.js
+++ pages/housestype/housestype.js
@@ -80,7 +80,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getProjectHouserholdList(options)
+    this.getProjectHouserholdList(options)    //获取户型列表
   },
   //获取户型列表
   getProjectHouserholdList(options){
@@ -90,20 +90,35 @@ Page({
     $http(apiSetting.projectApiFindProjectHouserholdListById, promise).then((data) => {
       let hourserholdlist = data.data;
       if (!hourserholdlist) return
+      this.setData({ allhourseList: hourserholdlist})
       this.getHourseTypeList(hourserholdlist)
+      let houserHoldFileLength = hourserholdlist.length
+      this.getProjectHouserholdFileList(houserHoldFileLength)
     }), (error) => {
       console.log(error)
     }
   },
 
+  
+  //查询户型分类列表
+  getHourseTypeList(hourserholdlist){
+    let promise = { dictname: "户型分类"}
+    $http(apiSetting.projectApiFindOtherDictValues, promise).then((data) => {
+      let _arr=data.data.split(',')
+      this.setData({ hourseTypeList:_arr})
+      this.getHourseList(hourserholdlist)
+    }), (error) => {
+      console.log(error)
+    }
+  },
   //户型列表数据格式整理
-  getHourseList(hourserholdlist){
+  getHourseList(hourserholdlist) {
     // let hourselist = JSON.parse(options.hourselist)
-    let hourselist = hourserholdlist
+    let hourselist = hourserholdlist            //户型数组
     //this.setData({ allhourseList: hourselist, hourseViewList:hourselist })     //赋值给全部户型列表
-    let _arr0=[]      //全部户型数组
-    let _arr1=[]      //一室临时数组
-    let _arr2=[]      //二室临时数组
+    let _arr0 = []      //全部户型数组
+    let _arr1 = []      //一室临时数组
+    let _arr2 = []      //二室临时数组
     let _arr3 = []    //三室临时数组
     let _arr4 = []    //四室临时数组
     let _arr5 = []    //其他房型临时数组
@@ -115,87 +130,49 @@ Page({
     // let point4 = []     //四室亮点
     // let point5 = []     //其他房型亮点
     // hourselist
-    for (let i = 0; i < hourselist.length;i++){
+    for (let i = 0; i < hourselist.length; i++) {
       //绑定优势卖点
       let _point = hourselist[i].buyingpoint
-      if (_point){
-        _point=_point.split(',')
+      if (_point) {
+        _point = _point.split(',')
       }
-      hourselist[i].buyingpoint=_point
-      if (hourselist[i].housetype == this.data.hourseTypeList[0]){    //一室
+      hourselist[i].buyingpoint = _point
+      _arr0 = hourselist
+      if (hourselist[i].housetype == this.data.hourseTypeList[0]) {    //一室
         _arr1.push(hourselist[i])
-        _arr0.push(hourselist[i])
-      } else if (hourselist[i].housetype == this.data.hourseTypeList[1]){   //二室
+        // _arr0.push(hourselist[i])
+      } else if (hourselist[i].housetype == this.data.hourseTypeList[1]) {   //二室
         _arr2.push(hourselist[i])
-        _arr0.push(hourselist[i])
+        // _arr0.push(hourselist[i])
       } else if (hourselist[i].housetype == this.data.hourseTypeList[2]) {    //三室
         _arr3.push(hourselist[i])
-        _arr0.push(hourselist[i])
+        // _arr0.push(hourselist[i])
       } else if (hourselist[i].housetype == this.data.hourseTypeList[3]) {    //四室
         _arr4.push(hourselist[i])
-        _arr0.push(hourselist[i])
+        // _arr0.push(hourselist[i])
       } else if (hourselist[i].housetype == this.data.hourseTypeList[4]) {    //其他
         _arr5.push(hourselist[i])
-        _arr0.push(hourselist[i])
+        // _arr0.push(hourselist[i])
       }
     }
     this.setData({
-      allhourseList:_arr0,
-      // hourseViewList:_arr0,
-      onehourse:_arr1,
-      twohourse:_arr2,
-      threehourse:_arr3,
-      fourhourse:_arr4,
-      otherhourse:_arr5
-      })
-  
-    // let typeList = this.data.hourseTypeList
-    // for (let j = 0; j < hourselist.length; j++) {
-    //   if (hourselist[j].housetype == typeList[0]) {
-    //     _arr1.push(hourselist[j])                                       //一室
-    //     point.push(hourselist[j].buyingpoint.split(','))
-    //     point1.push(hourselist[j].buyingpoint.split(','))               //一室优势
-    //   } else if (hourselist[j].housetype == typeList[1]){
-    //     _arr2.push(hourselist[j])                                        //二室
-    //     point.push(hourselist[j].buyingpoint.split(','))
-    //     point2.push(hourselist[j].buyingpoint.split(','))                //二室优势
-    //   } else if (hourselist[j].housetype == typeList[2]){
-    //     _arr3.push(hourselist[j])                                       //三室
-    //     point.push(hourselist[j].buyingpoint.split(','))
-    //     point3.push(hourselist[j].buyingpoint.split(','))               //三室优势
-    //   } else if (hourselist[j].housetype == typeList[3]) {
-    //     _arr4.push(hourselist[j])                                       //四室
-    //     point.push(hourselist[j].buyingpoint.split(','))
-    //     point4.push(hourselist[j].buyingpoint.split(','))               //四室优势
-    //   } else if (hourselist[j].housetype == typeList[4]) {
-    //     _arr5.push(hourselist[j])                                       //其他
-    //     point.push(hourselist[j].buyingpoint.split(','))
-    //     point5.push(hourselist[j].buyingpoint.split(','))               //其他优势
-    //   } else{
-    //     point.push(hourselist[j].buyingpoint.split(','))                  //全部优势
-    //   }
-    // }
+      allhourseList: _arr0,
+      hourseViewList: _arr0,
+      onehourse: _arr1,
+      twohourse: _arr2,
+      threehourse: _arr3,
+      fourhourse: _arr4,
+      otherhourse: _arr5
+    })
 
     // this.setData({ onehourse: _arr1, twohourse: _arr2, threehourse: _arr3, fourhourse: _arr4, otherhourse:_arr5})
     // this.setData({ allPointList: point, pointViewList: point, onePointList: point1, twoPointList: point2, threePointList: point3, fourPointList: point4, otherPointList:point5})
-   
+
     //查询户型图片列表
     // for (let i = 0; i < hourselist.length; i++){
     //   this.getProjectHouserholdFileList(hourselist[i].id)
     // }
-    let houserHoldFileLength = hourselist.length
-    this.getProjectHouserholdFileList(houserHoldFileLength)
-  },
-  //查询户型分类列表
-  getHourseTypeList(hourserholdlist){
-    let promise = { dictname: "户型分类"}
-    $http(apiSetting.projectApiFindOtherDictValues, promise).then((data) => {
-      let _arr=data.data.split(',')
-      this.setData({ hourseTypeList:_arr})
-      this.getHourseList(hourserholdlist)
-    }), (error) => {
-      console.log(error)
-    }
+
   },
 
   //通过id获取户型图片列表
@@ -212,38 +189,44 @@ Page({
 //     }
 //     console.log("显示列表：", _arr)
 //   },
-
+//查询户型图片
   getProjectHouserholdFileList(imgListLength) {
-    let _t=this.data.t
-    if (_t >= imgListLength){
+    let _t=this.data.t              //_t=0
+
+    if (_t > imgListLength - 1) {
       let _arr1 = this.data._hourseImgList
       for (let i = 0; i < _arr1.length; i++) {
         for (let j = 0; j < _arr1[i].length; j++) {
-          if (_arr1[i][j]!==null){
+          if (_arr1[i][j] !== null) {
             _arr1[i][j].upload_file_path = this.data.imgpath + _arr1[i][j].upload_file_path
-          }else{
-            _arr1[i][j] = {upload_file_path: this.data.imgpath} 
+          } else {
+            _arr1[i][j] = { upload_file_path: this.data.imgpath }
           }
         }
       }
       //将图片挂在到户型列表上
       let allList = this.data.allhourseList
-      for (let i = 0; i < allList.length;i++){
+      for (let i = 0; i < allList.length; i++) {
         allList[i].imgArr = _arr1[i]
       }
-      this.setData({ allhourseList: allList, hourseViewList: allList})
+      this.setData({ allhourseList: allList, hourseViewList: allList })
       return
     }  
-    let promise = { houserhold_id: this.data.allhourseList[_t].id }
+
+
+
+    let _allhourseList = this.data.allhourseList
+    if (_t > imgListLength-1) return
+    let promise = { houserhold_id: _allhourseList[_t].id }
     let _arr = this.data._hourseImgList
-    
+
     $http(apiSetting.projectApiFindProjectHouserholdFileListById, promise).then((data) => {
-      _arr.push(data.data)
-      this.setData({ _hourseImgList: _arr,t:_t+1})
-      this.getProjectHouserholdFileList(imgListLength)
+        _arr.push(data.data)
+        this.setData({ _hourseImgList: _arr, t: _t + 1 })
+        this.getProjectHouserholdFileList(imgListLength)
     }), (error) => {
       console.log(error)
-    }
+    }    
   },
   //户型图页图片错误
   erroImage1(e){
